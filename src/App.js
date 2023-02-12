5de96c513c708b143fa6534a4aca6f814bbd46b2
Princess Pocon <pocon.evangeline@gmail.com>
2023-02-12 16:50:49 +0800

    lift the state up for time board

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
+import { useState } from "react";
 import { TimeBoard } from "./components/time/TimeBoard";
 
 function App() {
+  const [page, setPage] = useState(1);
+  const [userHr, setUserHr] = useState("00");
+  const [userMin, setUserMin] = useState("00");
+  const [userSec, setUserSec] = useState("00");
 
+  const [totalSec, setTotalSec] = useState("00");
+  const [totalMin, setTotalMin] = useState("00");
+  const [totalHr, setTotalHr] = useState("00");
+
+  const [percentage, setPercentage] = useState(0);
+
+  const handleTimeChange = (e,state) => {
+      let length = e.target.value.length;
+      if (length < 3) {
+          state(e.target.value);
+      }
+  };
+
+  const computeTotalTime = () => {
+    setPage(2);
+    let totalUserTime = parseInt(userHr) * 3600 + parseInt(userMin) * 60 + parseInt(userSec);
+    let TotalRT = parseInt(totalHr) * 3600 + parseInt(totalMin) * 60 + parseInt(totalSec);
+    if (totalSec === "00" && totalMin === "00" && totalHr === "00"  ) {
+        alert("Please fill in all the fields");
+        return;
+    }
+    if (userSec === "" && userMin === "" && userHr === "") {
+        alert("Please fill in all the fields");
+        return;
+    }
+
+    if (totalUserTime > TotalRT) {
+        alert("Your total time is greater than the total RT time");
+        return;
+    }
+    let percentage = (totalUserTime / TotalRT) * 100;
+    let percentageRounded = Math.round(percentage * 100) / 100;
+    setPercentage(percentageRounded);
+
+}
   return (
-    <div className="font-sans">
-      <TimeBoard />
+    <div className="font-sans flex justify-center h-screen items-center">
+      {
+        page === 1 ? <div className="flex flex-col items-center">
+          <TimeBoard 
+            userHr={userHr} handleUserHrChange={(e)=> {handleTimeChange(e, setUserHr)}}
+            userMin={userMin} handleUserMinChange={(e)=> {handleTimeChange(e, setUserMin)}}
+            userSec={userSec} handleUserSecChange={(e)=> {handleTimeChange(e, setUserSec)}}
+            totalHr={totalHr} handleTotalHrChange={(e)=> {handleTimeChange(e, setTotalHr)}}
+            totalMin={totalMin} handleTotalMinChange={(e)=> {handleTimeChange(e, setTotalMin)}}
+            totalSec={totalSec} handleTotalSecChange={(e)=> {handleTimeChange(e, setTotalSec)}}
+            computeTotalTime={computeTotalTime}
+          />
+        </div> : 
+        <div><p>Hello</p></div>
+      }
+      
     </div>
   );
 }
--- src/components/time/TimeBoard.js
+++ src/components/time/TimeBoard.js
@@ -5,70 +5,32 @@ import { TimeLabel } from "./TimeLabel";
 import { TimeBoardName } from "./TimeBoardName";
 import { ProgressBar } from "../result/ProgressBar"; 
 
-export const TimeBoard = () => {
-    const [userHr, setUserHr] = useState("00");
-    const [userMin, setUserMin] = useState("00");
-    const [userSec, setUserSec] = useState("00");
-
-    const [totalSec, setTotalSec] = useState("00");
-    const [totalMin, setTotalMin] = useState("00");
-    const [totalHr, setTotalHr] = useState("00");
-
-    const [percentage, setPercentage] = useState(0);
-
-    const handleTimeChange = (e,state) => {
-        let length = e.target.value.length;
-        if (length < 3) {
-            state(e.target.value);
-        }
-    };
-
-    const computeTotalTime = () => {
-        let totalUserTime = parseInt(userHr) * 3600 + parseInt(userMin) * 60 + parseInt(userSec);
-        let TotalRT = parseInt(totalHr) * 3600 + parseInt(totalMin) * 60 + parseInt(totalSec);
-        if (totalSec === "00" && totalMin === "00" && totalHr === "00"  ) {
-            alert("Please fill in all the fields");
-            return;
-        }
-        if (userSec === "" && userMin === "" && userHr === "") {
-            alert("Please fill in all the fields");
-            return;
-        }
-
-        if (totalUserTime > TotalRT) {
-            alert("Your total time is greater than the total RT time");
-            return;
-        }
-        let percentage = (totalUserTime / TotalRT) * 100;
-        let percentageRounded = Math.round(percentage * 100) / 100;
-        setPercentage(percentageRounded);
-
-    }
+export const TimeBoard = ({
+    userHr,userMin,userSec,totalHr,totalMin,totalSec,computeTotalTime,
+    handleUserHrChange,handleUserMinChange,handleUserSecChange,
+    handleTotalHrChange,handleTotalMinChange,handleTotalSecChange
+}) => {
+    
     return (
-        <div className="flex flex-col p-4 ">
+        <div className="flex flex-col p-4 bg-sky-100 justify-center items-center">
             <div>
                 <TimeBoardName name="Your RT" />
                 <div className="flex items-center bg-161614 p-4 rounded-xl">
-                    <TimeInputWithLabel unit="HR" time={userHr} handleTimeChange={(e)=> {handleTimeChange(e, setUserHr)}}/>
-                    <TimeInputWithLabel unit="MIN" time={userMin} handleTimeChange={(e)=> {handleTimeChange(e, setUserMin)}}/>
-                    <TimeInputWithLabel unit="SEC" time={userSec} handleTimeChange={(e)=> {handleTimeChange(e, setUserSec)}}/>
+                    <TimeInputWithLabel unit="HR" time={userHr} handleTimeChange={(e)=> {handleUserHrChange(e)}}/>
+                    <TimeInputWithLabel unit="MIN" time={userMin} handleTimeChange={(e)=> {handleUserMinChange(e)}}/>
+                    <TimeInputWithLabel unit="SEC" time={userSec} handleTimeChange={(e)=> {handleUserSecChange(e)}}/>
                 </div>
             </div>
             <div>
                 <TimeBoardName name="Total RT" />
                 <div className="flex items-center bg-161614 p-4 rounded-xl">
-                    <TimeInputWithLabel unit="HR" time={totalHr} handleTimeChange={(e)=> {handleTimeChange(e, setTotalHr)}}/>
-                    <TimeInputWithLabel unit="MIN" time={totalMin} handleTimeChange={(e)=> {handleTimeChange(e, setTotalMin)}}/>
-                    <TimeInputWithLabel unit="SEC" time={totalSec} handleTimeChange={(e)=> {handleTimeChange(e, setTotalSec)}}/>
+                    <TimeInputWithLabel unit="HR" time={totalHr} handleTimeChange={(e)=> {handleTotalHrChange(e)}}/>
+                    <TimeInputWithLabel unit="MIN" time={totalMin} handleTimeChange={(e)=> {handleTotalMinChange(e)}}/>
+                    <TimeInputWithLabel unit="SEC" time={totalSec} handleTimeChange={(e)=> {handleTotalSecChange(e)}}/>
                 </div>
             </div>
         <button onClick={computeTotalTime} className="bg-d0f4de text-161614 font-bold rounded-md px-4 py-2 ml-4 ">See my progress! ✨</button>
-        <div>
-            <p className=" font-bold text-2xl">{percentage}%</p>
-        </div>
-        <ProgressBar percentage={percentage} />
-            
-        </div>
+       </div>
     );
 }
 
