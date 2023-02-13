b4ba7d9aea84c7515f110c23be0c7f89b3c3387e
Princess Pocon <pocon.evangeline@gmail.com>
2023-02-13 17:11:52 +0800

    center progress bar content

--- src/components/result/ProgressBar.js
+++ src/components/result/ProgressBar.js
@@ -1,12 +1,12 @@
 export const ProgressBar = ({ percentage }) => {
     return (
-        <div className="flex  bg-161614 dark:bg-gray-600 h-10 rounded-full w-full">
+        <div className="flex  bg-161614 dark:bg-gray-600 h-10 items-center rounded-full w-full">
             
             {
-                percentage < 2 ? <div className={`rounded-full bg-d0f4de h-3 w-1 mt-3` } style={{width: `${1}%`}}></div> 
-                : percentage < 4 ? <div className={`rounded-full bg-d0f4de h-6 mt-2` } style={{width: `${2}%`}}></div>
+                percentage < 2 ? <div className={`rounded-full bg-d0f4de h-3 w-1 ` } style={{width: `${1}%`}}></div> 
+                : percentage < 4 ? <div className={`rounded-full bg-d0f4de h-6 ` } style={{width: `${2}%`}}></div>
                 : 
-                <div className={`rounded-full bg-d0f4de py-4` } style={{width: `${percentage}%`}}></div>
+                <div className={`rounded-full bg-d0f4de h-10` } style={{width: `${percentage}%`}}></div>
             }
         </div>
     )
