bf72e9c521cc5d65fe507eaf5995072dfd50849a
Princess Pocon <pocon.evangeline@gmail.com>
2023-02-13 16:45:20 +0800

    add padding bottom to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,7 @@ import {RiGithubFill} from 'react-icons/ri'
 
 export const Footer = () => {
     return (
-        <div className="flex justify-center items-center  dark:text-white px-2">
+        <div className="flex justify-center items-center  dark:text-white px-2 pb-4">
             <p className='flex items-center text-sm'>Created by  <a href="https://github.com/prxncxss03" className="underline px-2"> Princess Pocon</a> <RiGithubFill></RiGithubFill></p>
         </div>
     )
