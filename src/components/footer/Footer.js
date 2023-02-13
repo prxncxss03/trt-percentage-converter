b3e6e2412a7f9bd258e5e4ae1b69a4fc8d99de05
Princess Pocon <pocon.evangeline@gmail.com>
2023-02-13 13:55:10 +0800

    add github icon to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,9 @@
+import {RiGithubFill} from 'react-icons/ri'
+
 export const Footer = () => {
     return (
         <div className="flex justify-center items-center  dark:text-white absolute bottom-4 px-2">
-            <p>Created by <a href="https://github.com/prxncxss03" className="underline">Princess Pocon</a> 💀</p>
+            <p className='flex items-center'>Created by  <a href="https://github.com/prxncxss03" className="underline px-2"> Princess Pocon</a> <RiGithubFill></RiGithubFill></p>
         </div>
     )
 }
