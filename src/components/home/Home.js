183519d4ff97a1d6aec1e2f6a5145ef16201d925
Princess Pocon <pocon.evangeline@gmail.com>
2023-02-14 14:51:35 +0800

    add home

---
+++ src/components/home/Home.js
@@ -0,0 +1,9 @@
+import { HomeBtn } from "./HomeBtn"
+export const Home = () => {
+    return(
+        <div>
+            <HomeBtn name="RT to Percentage"/>
+            <HomeBtn name="Percentage to RT"/>
+        </div>
+    )
+}
--- src/components/home/HomeBtn.js
+++ src/components/home/HomeBtn.js
@@ -1,10 +1,9 @@
 import { RiHome3Fill } from "react-icons/ri"
-export const HomeBtn = () => {
+export const HomeBtn = ({name}) => {
     return(
-        <Link to="/">
-            <button className="bg-focus:outline-none">
-                <RiHome3Line size={30}/>
-            </button>
-        </Link>
+        <button className="bg-fcf6bd mt-4 text-161614 font-bold md:w-1/2 lg:w-5/12 rounded-md px-4 py-2 w-full shadow-sm">
+            <span className="mr-2">{name}</span>
+            <RiHome3Fill size={20}/>
+        </button>
     )
 }   
