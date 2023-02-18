4e066e8da29b3474d5243ee1103e7747de8bdddd
Princess Pocon <pocon.evangeline@gmail.com>
2023-02-18 12:11:17 +0800

    fix 00 format if less than ten in the result2

--- src/components/time/TimeBoardStatic.js
+++ src/components/time/TimeBoardStatic.js
@@ -10,9 +10,9 @@ export const TimeBoardStatic = ({
         <div className="w-full flex flex-col ">
                 <BoardName name={name} />
                 <div className="flex items-center bg-161614 dark:bg-gray-600 p-4 rounded-xl">
-                    <TimeInputWithLabel time={timeHR} handleTimeChange={()=>{}} unit="HR" />
-                    <TimeInputWithLabel time={timeMIN} handleTimeChange={()=>{}} unit="MIN" />
-                    <TimeInputWithLabel time={timeSEC} handleTimeChange={()=>{}} unit="SEC" />
+                    <TimeInputWithLabel time={timeHR < 10 ? `0${timeHR}`: timeHR} handleTimeChange={()=>{}} unit="HR" />
+                    <TimeInputWithLabel time={timeMIN < 10 ? `0${timeMIN}`: timeMIN} handleTimeChange={()=>{}} unit="MIN" />
+                    <TimeInputWithLabel time={timeSEC < 10 ? `0${timeSEC}`: timeSEC} handleTimeChange={()=>{}} unit="SEC" />
                 </div>
         </div>
     )
