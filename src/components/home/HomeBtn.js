384478c78ab8d7f5ae05be2d151ae2e2381e498c
Princess Pocon <pocon.evangeline@gmail.com>
2023-02-22 16:33:14 +0800

    add hover effect to home btn

--- src/components/home/HomeBtn.js
+++ src/components/home/HomeBtn.js
@@ -4,7 +4,7 @@ import {TbNumbers} from "react-icons/tb"
 export const HomeBtn = ({name, handleChangePage}) => {
     return(
         <button onClick={(e) => handleChangePage(e)}
-        className="bg-fcf6bd my-1 flex items-center justify-center text-161614 font-bold  rounded-md px-4 py-2 w-full shadow-sm">
+        className="hover:bg-transparent bg-fcf6bd hover:border-fcf6bd border-transparent border hover:border my-1 flex items-center justify-center hover:text-fcf6bd text-161614 font-bold  rounded-md px-4 py-2  w-full shadow-sm">
             <span className="mr-2">{name}</span>
             {
             name === "RT to Percentage" ? <RiPercentFill /> : name === "Go Home" ? <RiHome3Fill/> : <TbNumbers />
