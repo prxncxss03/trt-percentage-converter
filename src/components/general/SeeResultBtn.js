cc5adf40379b5f85519221920610d11b61df41f7
Princess Pocon <pocon.evangeline@gmail.com>
2023-02-22 16:35:28 +0800

    add hover effect to see result

--- src/components/general/SeeResultBtn.js
+++ src/components/general/SeeResultBtn.js
@@ -2,7 +2,7 @@ import {RiArrowRightLine} from 'react-icons/ri'
 export const SeeResultBtn = ({name, onClick}) => {
     return(
         <button onClick={onClick}
-        className="bg-d0f4de shadow-sm mt-4  text-161614 font-bold rounded-md px-4 py-2 flex w-full items-center justify-center mb-1">
+        className="bg-d0f4de hover:bg-transparent hover:border-d0f4de shadow-sm mt-4 border-transparent border text-161614 hover:text-d0f4de font-bold rounded-md px-4 py-2 flex w-full items-center justify-center mb-1">
             <span className='mr-2'>{name} </span>
             <RiArrowRightLine />
         </button>
